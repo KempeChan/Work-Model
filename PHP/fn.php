2d864545327745ad5c73812ff1a05d3d529f2525
Kempe <569576618@qq.com>
2012-05-15 15:20:18 +0800

    添加从数组到(1,1),(1,2),(1,3) 格式的转换函数，用于一次性插入多条记录

--- PHP/fn.php
+++ PHP/fn.php
@@ -9,6 +9,23 @@ class fn{
 		print_r($data);
 		echo "</pre>";
 	}
+	
+	public static insertOnce($arr,$otherFieldValue){
+              # 字符串构造函数，可以由专门的类管理
+              $insertValue = '';
+              foreach($arr as $key => $v){
+                       
+                       if($v != NULL){
+                                $insertValue .= "($otherFieldValue,$v)"; 
+                                if(count($arr)-1 != $key){
+                                          $insertValue .= ",";
+                                 }
+                       }
+              }
+              # 输出格式: (1,1),(1,2),(1,3)
+              # INSERT INTO template_department_map(template_id,department_id) VALUES(1,2),(1,3),(2,3);
+
+	}
 
 
 }
